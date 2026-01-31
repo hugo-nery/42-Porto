3c0e4551e09ce605ecb7a20bf5a334beef59108a
hugo-nery <hugoalbuquerquen@gmail.com>
2026-01-31 12:21:45 +0000

    c1-07

--- 42-Piscine/C01/ex07/ft_rev_int_tab.c
+++ 42-Piscine/C01/ex07/ft_rev_int_tab.c
@@ -6,70 +6,68 @@
 /*   By: hde-albu <hde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 10:32:19 by hde-albu          #+#    #+#             */
-/*   Updated: 2026/01/30 13:58:57 by hde-albu         ###   ########.fr       */
+/*   Updated: 2026/01/31 12:21:00 by hde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-#include <stdio.h>
+// #include <stdio.h>
 
-void ft_rev_int_tab(int *tab, int size);
+// void ft_rev_int_tab(int *tab, int size);
 
-int main () {
+// int main () {
+//     int n_lst[] = {1,2,3,4,5,6,7,8,9,10,11};
+//     int len = sizeof(n_lst) / sizeof(n_lst[0]);
 
-    int n_lst[] = {1,2,3,4};
+//     ft_rev_int_tab(n_lst, len);
 
-    int size = sizeof(n_lst) / sizeof(n_lst[0]);
+// 	int i = 0;
+// 	while (i < len) {
+// 		printf("%d,", n_lst[i]);
+// 		i++;
+// 	}
 
-    int i = 0;
-    while (i < size){
-        printf("%d", n_lst[i]);
-        i++;
-    }
+//     return 0;
+// }
 
-    printf("---------------%d--------------",i);
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+	int	last;
 
-    // printf("\nantes\n");
-
-    // ft_rev_int_tab(n_lst, i);
-
-    // printf("\ndepois\n");
-    // i = 0;
-    // while (n_lst[i] != '\0'){
-    //     printf("%d", n_lst[i]);
-    //     i++;
-    // }
-
-    return 0;
+	i = 0;
+	temp = 0;
+	last = size - 1;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[last];
+		tab[last] = temp;
+		i++;
+		last--;
+	}
 }
 
-void ft_rev_int_tab(int *tab, int size) {
+// void	ft_rev_int_tab(int *tab, int size)
+// {
+// 	int	temp_list [size];
+// 	int	i;
 
-    int temp_list [size];
+// 	size -= 1;
+// 	i = 0;
+// 	while (i <= size)
+// 	{
+// 		temp_list[i] = tab[size - i];
+// 		i++;
+// 	}
+// 	i = 0;
+// 	while (i <= size)
+// 	{
+// 		tab[i] = temp_list[i];
+// 		i++;
+// 	}
+// }
 
-    int i = 0;
-    while (i <= size) {
-
-        tab[i] = temp_list[size - i];
-        
-        // printf("i_%d - tab = %d\t", i, tab[i]);
-        // printf("temp = %d\n", temp_list[i]);
-
-        i++;
-    }
-
-    printf("\n\n");
-
-    i = 0;
-    while (i <= size) {
-        
-        printf("i_%d - tab = %d\t", i, tab[i]);
-        printf("temp = %d\n", temp_list[i]);
-
-        i++;
-    }
-
-}
-
-//cc  -Wall -Wextra -Werror ft
+//cc -Wall -Wextra -Werror ft
