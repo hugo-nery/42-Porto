b91ce9d96db3001f2ca871c0eb40a6bfcf6dc315
hugo-nery <hugoalbuquerquen@gmail.com>
2026-01-31 19:59:32 +0000

    C02

--- 42-Piscine/C02/ex00/ft_strcpy.c
+++ 42-Piscine/C02/ex00/ft_strcpy.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-albu <hde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/29 21:40:08 by hde-albu          #+#    #+#             */
-/*   Updated: 2026/01/31 19:59:07 by hde-albu         ###   ########.fr       */
+/*   Created: 2026/01/31 17:12:26 by hde-albu          #+#    #+#             */
+/*   Updated: 2026/01/31 17:44:32 by hde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,39 @@
 
 #include <stdio.h>
 
-void ft_putstr(char *str);
+char *ft_strcpy(char *dest, char *src);
 
 int main () {
-    char name [] = "hugo de a s nery";
-	ft_putstr(name);
+
+    char name[] = "hugo nery";
+
+    int len = sizeof(name) / sizeof(name[0]);
+
+    // printf("%d",len);
+
+    char my_arr[len];
+    
+    ft_strcpy(name, my_arr);
+
+    int i = 0;
+    while(my_arr[i] != '\0'){
+        printf("%c", my_arr[i]);
+        i++;
+    }
 
     return 0;
 }
 
-void	ft_putstr(char *str)
-{
-	int	i;
+char *ft_strcpy(char *dest, char *src) {
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+    int i = 0;
+    while(src[i] != '\0'){
+        printf("%c", src[i]);
+        dest[i] = src[i];
+        i++;
+    }
+
+    return dest;
 }
 
 // cc  -Wall -Wextra -Werror
