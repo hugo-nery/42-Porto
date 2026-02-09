ed75fabdd529b7c1e2fd7f21108ccf9decb0ea27
hugo-nery <hugoalbuquerquen@gmail.com>
2026-02-09 22:07:03 +0000

    finshed C05

--- 42-Piscine/C05/ex07/ft_find_next_prime.c
+++ 42-Piscine/C05/ex07/ft_find_next_prime.c
@@ -1,33 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hde-albu <hde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/09 21:13:46 by hde-albu          #+#    #+#             */
-/*   Updated: 2026/02/09 21:25:30 by hde-albu         ###   ########.fr       */
+/*   Created: 2026/02/09 21:25:45 by hde-albu          #+#    #+#             */
+/*   Updated: 2026/02/09 22:05:33 by hde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <unistd.h>
 // #include <stdio.h>
 
-// int ft_is_prime(int nb);
+// int	ft_is_prime(int nb);
+// int ft_find_next_prime(int nb);
 
 // int main () {
 
-// 	int i = 0;
+//     printf ("%d", ft_find_next_prime(8));
 
-// 	while (i <= 80)
-// 	{
-// 		if (ft_is_prime(i))
-// 			printf ("%d\n", i);
-// 		i++;
-// 	}
-
-// 	return 0;
+//     return 0;
 // }
+
+int	ft_find_next_prime(int nb)
+{
+	while (!ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
 
 int	ft_is_prime(int nb)
 {
